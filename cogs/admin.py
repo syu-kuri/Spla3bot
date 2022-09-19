f057a3684956b2de8b796606cc21ab825886263c
syukuri <souki.shinohara@outlook.jp>
2022-09-20 03:09:40 +0900

    Changed to hide commands for Admin

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,18 +7,16 @@ class AdminCog(Cog, name="Bot製作者のみ使用可能コマンド"):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @commands.hybrid_command(name="reload", description="CogのReload", with_app_command=True)
+    @commands.command(name="reload", hidden=True)
     @commands.is_owner()
-    async def reload(self, ctx: discord.Interaction, extension):
-        """【管理者限定】CogのReload"""
-        await self.bot.reload_extension(f"cogs.{extension}")
+    async def reload(self, inter, extension):
+        self.bot.reload_extension(f"cogs.{extension}")
         embed = discord.Embed(title='リロード成功', description=f'{extension} Cog をリロードしました。', color=0xff00c8)
-        await ctx.reply(embed=embed)
+        await inter.reply(embed=embed)
 
-    @commands.hybrid_command(name="get_error", description="CommandErrorの取得", with_app_command=True)
+    @commands.command(name="get_error", hidden=True)
     @commands.is_owner()
     async def get_error(self, ctx, error_id):
-        """【管理者限定】CommandErrorの取得"""
         ch = 982915045891665940
         channel = self.bot.get_channel(ch)
         msg = await channel.fetch_message(error_id)
@@ -28,7 +26,9 @@ class AdminCog(Cog, name="Bot製作者のみ使用可能コマンド"):
         fields = dict['fields']
         emb = discord.Embed(title=dict['title'] + "取得", description="", color=dict['color'])
         for field in fields:
+            # print(field['name'], field['value'], field['inline'])
             emb.add_field(name=field['name'], value=field['value'], inline=field['inline'])
+        # asyncio.get_event_loop().run_until_complete(await ctx.reply(embed=emb))
         await ctx.reply(embed=emb)
 
 
