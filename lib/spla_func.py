0a00f83e9bfe3cd881f5cc184459d9b49297b731
syu-kuri <72872356+syu-kuri@users.noreply.github.com>
2026-06-11 19:17:47 +0900

    Fix coop grouping stage handling

--- lib/spla_func.py
+++ lib/spla_func.py
@@ -3,12 +3,15 @@ from lib.check import *
 from lib.molding import *
 
 
+COOP_RULES = {"coop-grouping", "coop-grouping-regular"}
+
+
 def spla3(rule, time_slot):
     if time_slot != "schedule":
-        if rule != "coop-grouping-regular":
-            base = get_schedule(rule, time_slot)
-            data = check_fest(base, time_slot)
-        else:
+        if rule in COOP_RULES:
             base = get_schedule(rule, time_slot)
             data = common_coop_molding(base)
+        else:
+            base = get_schedule(rule, time_slot)
+            data = check_fest(base, time_slot)
     return data
